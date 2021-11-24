a5716ae869487e67a3a4edc27530f70290db7f8e
Lukas Klinghammer; eXXcellent solutions <Lukas.Klinghammer@exxcellent.de>
2021-11-24 18:47:30 +0100

    added ID to the toHash Method of the book. This allows a book to be removed more precisely, otherwise always the first book that matches would had been removed.

--- src/main/java/org/assessment/adesso/library/impl/books/Book.java
+++ src/main/java/org/assessment/adesso/library/impl/books/Book.java
@@ -82,7 +82,8 @@ public class Book {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
 		Book book = (Book) o;
-		return Objects.equals(title, book.title) &&
+		return id == book.id &&
+				Objects.equals(title, book.title) &&
 				Objects.equals(author, book.author) &&
 				genre == book.genre;
 	}
