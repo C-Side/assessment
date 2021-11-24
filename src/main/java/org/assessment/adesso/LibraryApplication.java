57d28dcee1c073e4aca60f578e5a0374c64ec2a0
Lukas Klinghammer; eXXcellent solutions <Lukas.Klinghammer@exxcellent.de>
2021-11-24 14:56:57 +0100

    basic project setup. using gradle and spring boot

---
+++ src/main/java/org/assessment/adesso/LibraryApplication.java
@@ -0,0 +1,16 @@
+package org.assessment.adesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Klinghammer, Lukas; eXXcellent solutions
+ * @version : 24.11.2021
+ **/
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+}
